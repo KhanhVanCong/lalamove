a0b640dff77dbf892b77c0c17dfe92f687dc7a8a
khanhvancong <khanhvc2310@gmail.com>
2023-11-15 14:26:16 +0700

    Refactor code resources city info

--- lib/lalamove/resources/city_info.rb
+++ lib/lalamove/resources/city_info.rb
@@ -3,33 +3,14 @@
 module Lalamove
   module Resources
     class CityInfo < ActiveService::Base
-      def initialize()
-        # @params = params
-        # @stock_location = params[:stock_location]
-        # @orders = params[:orders].to_a
-        # @service_type = params[:service_type] || 'LALAGO'
-      end
-
       def perform
         process
       end
 
       private
-
-      # attr_reader :params, :stock_location, :orders, :service_type
-
-      def process
-        Lalamove::Services::CityInfoService.perform!()
-      end
-
-      # def payload
-      #   {
-      #     language: Lalamove.configuration.country,
-      #     serviceType: service_type,
-      #     stops: delivery_stops,
-      #     specialRequests: []
-      #   }
-      # end
+        def process
+          Lalamove::Services::CityInfoService.perform!()
+        end
     end
   end
 end
